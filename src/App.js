8c95f71bad01f374e211fb8a3e1219314f6dd48a
michal <michal@dataquest.io>
2018-07-26 17:16:34 +0100

    #4: building board dynamically

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ function Square(props) {
 class Board extends React.Component {
   renderSquare(i) {
     return (
-      <Square
+      <Square key={i}
         value={this.props.squares[i]}
         onClick={() => this.props.onClick(i)}
       />
@@ -20,24 +20,12 @@ class Board extends React.Component {
   }
 
   render() {
+    let arr = [];
+    for (let i=0; i<this.props.squares.length; i++) {
+      arr.push(this.renderSquare(i));
+    }
     return (
-      <div>
-        <div className="board-row">
-          {this.renderSquare(0)}
-          {this.renderSquare(1)}
-          {this.renderSquare(2)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(3)}
-          {this.renderSquare(4)}
-          {this.renderSquare(5)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(6)}
-          {this.renderSquare(7)}
-          {this.renderSquare(8)}
-        </div>
-      </div>
+      <div className='GameGrid'>{arr}</div>
     );
   }
 }
