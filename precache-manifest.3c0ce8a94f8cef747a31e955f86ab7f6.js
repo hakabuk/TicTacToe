dafa175caa8144a9b2ad56e5914830bc74cfcf02
zurda <m.weizman@gmail.com>
2019-03-07 10:39:18 +0000

    Updates

---
+++ precache-manifest.3c0ce8a94f8cef747a31e955f86ab7f6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "bab3f4c4f0e95f942a60",
+    "url": "/React-TicTacToe/static/js/runtime~main.f383fec6.js"
+  },
+  {
+    "revision": "577feb3877cddf840b5f",
+    "url": "/React-TicTacToe/static/js/main.fd33d6a4.chunk.js"
+  },
+  {
+    "revision": "78e19b0f4b70dacdbb9a",
+    "url": "/React-TicTacToe/static/js/2.c705e599.chunk.js"
+  },
+  {
+    "revision": "577feb3877cddf840b5f",
+    "url": "/React-TicTacToe/static/css/main.cc750604.chunk.css"
+  },
+  {
+    "revision": "191019cc0d52ac344df233c3e5f66054",
+    "url": "/React-TicTacToe/index.html"
+  }
+];
